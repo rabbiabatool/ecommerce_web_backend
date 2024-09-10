f4e9740d66f12927738d11fcafee54380561e3a9
rabbiabatool <rabbiabatool875@gmail.com>
2024-09-10 23:54:52 +0500

    commit

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
 const userRoutes=require("./Routers/Routers");
-// const cors=require("cors");
+const cors=require('cors');
 
-// app.use(cors());
-
-app.use('/images', express.static('upload/images'));
-
-
-app.use(userRoutes);
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(4000, () => console.log("Server ready on port 4000."));
+
+
+// app.use('/images', express.static('upload/images'));
+
+
+app.use(userRoutes);
+
 
 
 
